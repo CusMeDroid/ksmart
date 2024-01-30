2b32328d54a30ff5e73ace5f17c52439b1af17ae
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-01-30 08:17:32 +0700

    Delete js directory

--- js/storage/index.js
+++
@@ -1,2 +0,0 @@
-var sessionEmail = localStorage.getItem("email") ? JSON.parse(localStorage.getItem("email")) : "";
-var sessionUid = localStorage.getItem("uid") ? JSON.parse(localStorage.getItem("uid")) : "";
