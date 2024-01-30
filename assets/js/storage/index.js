b48f76254a33e2d63e3f4358f02568d5d997d7e8
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-01-31 01:12:50 +0700

    Delete assets directory

--- assets/js/storage/index.js
+++
@@ -1,8 +0,0 @@
-/*
-    KSmart JavaScript
-    Copyright 2024 by CusMeDroid
-    DO NOT REMOVE THIS
-    Don't modify or duplicate without our permission, we are law-abiding citizens.
-*/
-var sessionEmail = localStorage.getItem("email") ? JSON.parse(localStorage.getItem("email")) : "";
-var sessionUid = localStorage.getItem("uid") ? JSON.parse(localStorage.getItem("uid")) : "";
