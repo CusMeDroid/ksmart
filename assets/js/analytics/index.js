75436bdfbf3a74418c63090489a39b6fb088eb3a
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-01-30 11:50:09 +0700

    Add files via upload

---
+++ assets/js/analytics/index.js
@@ -0,0 +1,8 @@
+/*
+    KSmart JavaScript
+    Copyright 2024 by CusMeDroid
+    DO NOT REMOVE THIS
+    Don't modify or duplicate without our permission, we are law-abiding citizens.
+*/
+import * as firebase from 'firebase/app';
+firebase.analytics();
