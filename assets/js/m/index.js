ec8a23da4a1169195a8bba11dd20a7a6b97168c9
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-01-30 13:39:51 +0700

    Update index.js

--- assets/js/m/index.js
+++ assets/js/m/index.js
@@ -33,5 +33,5 @@ document.getElementById("btnclose").onclick = function () {
   document.getElementById("edtdesk").style.display = "none";
 };
 document.getElementById("btnsys").onclick = function () {
-  window.location.href = "https://wa.me/6281932671715";
+  window.location.href = "https://wa.me/6281932671715?text=I'm%20interested%20to%20build%20%20system%20KSMART%20REMOTE%20SWITCH%0AWIFI_NAME:%0AWIFI_PASS:%0APlease%20configure%20my%20email%20"+sessionEmail;
 };
