7a2a8ed22ab9671f8317726ffdc95559343ee2d8
CusMeDroid <108187260+CusMeDroid@users.noreply.github.com>
2024-01-30 12:58:24 +0700

    Update index.js

--- assets/js/m/index.js
+++ assets/js/m/index.js
@@ -7,12 +7,21 @@
 document.getElementById("btnmenu").onclick = function () {
   document.getElementById("btnmenu").style.display = "none";
   document.getElementById("btnmenuclose").style.display = "block";
+  refRelay.on('value', (snapshot) => {
+    var dataRelay = snapshot.val();
+    if (dataRelay == "disable") {
+      document.getElementById("btnsys").style.display = "block";
+    } else {
+      document.getElementById("btnsys").style.display = "none";
+    }
+  });
   document.getElementById("btnedtdesc").style.display = "block";
   document.getElementById("btnlogout").style.display = "block";
 };
 document.getElementById("btnmenuclose").onclick = function () {
   document.getElementById("btnmenu").style.display = "block";
   document.getElementById("btnmenuclose").style.display = "none";
+  document.getElementById("btnsys").style.display = "none";
   document.getElementById("btnedtdesc").style.display = "none";
   document.getElementById("btnlogout").style.display = "none";
 };
@@ -21,4 +30,7 @@ document.getElementById("btnedtdesc").onclick = function () {
 };
 document.getElementById("btnclose").onclick = function () {
   document.getElementById("edtdesk").style.display = "none";
+};
+document.getElementById("btnsys").onclick = function () {
+  window.location.href = "https://wa.me/6281932671715";
 };
